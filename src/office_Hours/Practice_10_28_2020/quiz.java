04d0b8df9a399930ea780de1735b964d2189211a
Abdulaziz Ozden <abdulazizozden@Abdulaziz-MacBook-Pro.local>
2020-10-30 16:53:38 -0400

    unique characters and string proctice

--- src/office_Hours/Practice_10_28_2020/quiz.java
+++ src/office_Hours/Practice_10_28_2020/quiz.java
@@ -4,11 +4,10 @@ public class quiz {
 
     public static void main(String[] args) {
 
-        int x = 10; // x = 11+1 = 12;
-        int y = x++; // y = 10+1 = 11;
 
-        System.out.println(y++ + " " + x++ + " " + y);  // 10  11  11
-        //                  10 + " " +   11 +"  " + 11
+
+
+
 
 
     }
