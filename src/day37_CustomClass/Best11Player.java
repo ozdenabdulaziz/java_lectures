426ad2f3f5df2f63a98f7d955aae9143b960794b
Abdulaziz OZDEN <ozdenabdulaziz@gmail.com>
2020-12-04 22:55:18 -0500

    Practice for Custom Class

--- src/day37_CustomClass/Best11Player.java
+++ src/day37_CustomClass/Best11Player.java
@@ -13,13 +13,43 @@ public class Best11Player {
         Player DF4 = new Player();
         Player MD1 = new Player();
         Player MD2 = new Player();
-        Player LW  = new Player();
+        Player LW = new Player();
         Player RW = new Player();
-        Player FW = new Player();
-        Player FW2 = new Player();
+        Player CF1 = new Player();
+        Player CF2 = new Player();
 
-
-
+        GK.setInfo("Juventus", "Buffon", "GK", 1, LocalDate.of(1980, 01, 01), 8.9, 45);
+        GK.getInfo();
+        System.out.println("-------------------");
+        DF1.setInfo("Barcelona", "Puyol", "CB", 4, LocalDate.of(1981, 01, 01), 8.9, 55);
+        DF1.getInfo();
+        System.out.println("-------------------");
+        DF2.setInfo("Inter", "Cannavaro", "CB", 5, LocalDate.of(1973, 9, 13), 8.9, 25);
+        DF2.getInfo();
+        System.out.println("-------------------");
+        DF3.setInfo("Barcelona", "D.Alves", "RB", 2, LocalDate.of(1983, 5, 6), 8.6, 20);
+        DF3.getInfo();
+        System.out.println("-------------------");
+        DF4.setInfo("Real Madrid", "S.Ramos", "LB", 3, LocalDate.of(1986, 3, 30), 8.9, 35);
+        DF4.getInfo();
+        System.out.println("-------------------");
+        MD1.setInfo("Barcelona", "Xavi", "CM", 6, LocalDate.of(1980, 1, 25), 9.1, 50);
+        MD1.getInfo();
+        System.out.println("-------------------");
+        MD2.setInfo("Barcelona", "Iniesta", "CM", 8, LocalDate.of(1984, 5, 11), 9.0, 55);
+        MD2.getInfo();
+        System.out.println("-------------------");
+        LW.setInfo("Barcelona", "L.Messi", "LW", 10, LocalDate.of(1987, 6, 24), 9.5, 80);
+        LW.getInfo();
+        System.out.println("-------------------");
+        RW.setInfo("Manchester United", "G.Ronaldo", "RW", 7, LocalDate.of(1985, 2, 5), 9.5, 100);
+        RW.getInfo();
+        System.out.println("-------------------");
+        CF1.setInfo("Arsenal", "T.Henry", "CF", 9, LocalDate.of(1977, 8, 17), 8.9, 35);
+        CF1.getInfo();
+        System.out.println("-------------------");
+        CF2.setInfo("Barcelona", "S.Eto", "CF", 10, LocalDate.of(1981, 3, 10), 8.9, 45);
+        CF2.getInfo();
 
 
     }
--- src/day37_CustomClass/Player.java
+++ src/day37_CustomClass/Player.java
@@ -4,44 +4,35 @@ import java.time.LocalDate;
 
 public class Player {
 
-    public String playerTeam, playerName, playerPositions;
+    public String  playerName, playerPositions;
+    public String teamName;
     public int playerNumber, playerAge;
     public double playerRating, playerValue;
     public LocalDate dateOfBirth;
 
 
 
-    public void setInfo(String playerTeam, String playerName, String playerPositions, int playerNumber, LocalDate dateOfBirth, double playerRating, double playerValue){
+    public void setInfo(String teamName, String playerName, String playerPositions, int playerNumber, LocalDate dateOfBirth, double playerRating, double playerValue){
 
+        this.teamName = teamName;
         this.playerName = playerName;
         this.playerPositions = playerPositions;
         this.playerNumber = playerNumber;
-        playerAge = LocalDate.now().getYear() - dateOfBirth.getYear();
+        this.playerAge = LocalDate.now().getYear() - dateOfBirth.getYear();
         this.playerRating = playerRating;
         this.playerValue = playerValue;
         this.dateOfBirth = dateOfBirth;
 
     }
 
+
     public void getInfo(){
 
-        System.out.println("Team: " + playerTeam + "\nName: " + playerName + "\nPosition: " + playerPositions + "\nJersey Number: "  + playerNumber + "\nAge: " + playerAge + "\nRating: " + playerRating + "\nValue: " + playerValue);
+        System.out.println("Team: " + teamName + "\nName: " + playerName + "\nPosition: " + playerPositions + "\nJersey Number: "  + playerNumber + "\nAge: " + playerAge + "\nRating: " + playerRating + "\nValue: " + playerValue);
 
     }
 
 
-    @Override
-    public String toString() {
-        return "Player{" +
-                "playerTeam='" + playerTeam + '\'' +
-                ", playerName='" + playerName + '\'' +
-                ", playerPositions='" + playerPositions + '\'' +
-                ", playerNumber=" + playerNumber +
-                ", playerAge=" + playerAge +
-                ", playerRating=" + playerRating +
-                ", playerValue=" + playerValue +
-                '}';
-    }
 }
 
 
