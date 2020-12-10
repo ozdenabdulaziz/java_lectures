77c8f1a62a933df11fd243eef4bf6424dd7504fd
Abdulaziz OZDEN <ozdenabdulaziz@gmail.com>
2020-12-10 14:04:52 -0500

    Static

---
+++ src/day39_StaticBlock/StaticBlocks.java
@@ -0,0 +1,48 @@
+package day39_StaticBlock;
+
+
+import day37_CustomClass.Employee;
+
+public class StaticBlocks {
+
+    public static String company;
+    public static Employee employee1 ;
+    public static int b;
+    public static boolean isEmployeed;
+
+
+    public int a;
+
+    //xcelSheet
+
+    static{
+        company = "Capital One";
+        isEmployeed = true;
+        employee1 = new Employee();
+        employee1.setInfo("john", company, "SDET", "A123", "Male", 40, 55);
+        //
+        // a = 100;
+        b = 200;
+        System.out.println("Static Block");
+    }
+
+
+    /*
+    public static void main(String[] args) {
+        company = "Capital One";
+        isEmployeed = true;
+        employee1 = new Employee();
+        employee1.setInfo("john", company, "SDET", "A123", "Male", 40, 55);
+        b = 200;
+        System.out.println(company);
+        System.out.println(employee1);
+        System.out.println(b);
+        System.out.println(isEmployeed);
+    }
+*/
+
+
+
+
+
+}
---
+++ src/library/Data.java
@@ -0,0 +1,16 @@
+package library;
+
+public class Data {
+
+    public static String data1 = "A";
+    public static int data2 = 100;
+
+    public static void method1() {
+        System.out.println("Method 1");
+    }
+
+    public static void method2() {
+        System.out.println("Method 2");
+    }
+
+}
