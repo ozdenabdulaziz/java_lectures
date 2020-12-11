69de2617098ef5be9841506ecce3d37d94cbc2b3
Abdulaziz OZDEN <ozdenabdulaziz@gmail.com>
2020-12-11 16:37:22 -0500

    Constructor

--- src/library/Data.java
+++ src/library/Data.java
@@ -5,12 +5,19 @@ public class Data {
     public static String data1 = "A";
     public static int data2 = 100;
 
-    public static void method1() {
+    public static void method1(){
         System.out.println("Method 1");
     }
 
-    public static void method2() {
+    public static void method2(){
         System.out.println("Method 2");
+    }
+
+
+    public String data3, data4, data5, data6;
+
+    public void method3(){
+
     }
 
 }
