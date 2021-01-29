d7873619d30edec5f5995c0d560167dabf69845e
Abdulaziz OZDEN <ozdenabdulaziz@gmail.com>
2021-01-29 16:50:48 -0500

    collection Intro

--- src/IQ_CodingTasks_Solutions/Numbers_Odd_Even.java
+++ src/IQ_CodingTasks_Solutions/Numbers_Odd_Even.java
@@ -1,6 +1,34 @@
 package IQ_CodingTasks_Solutions;
 
 public class Numbers_Odd_Even {
+
+
+
+    public  static int identifyOddEven(int  n ) {
+
+        if(n%2 == 0){
+            System.out.println("You entered an even number: " + n);
+        }else{
+            System.out.println("You entered an odd number: " + n);
+        }
+
+        return n;
+    }
+
+    public  static  String  identifyOddEven2( int  n ) {
+
+        return  n%2==0 ? "Even" : "odd" ;
+
+    }
+
+
+
+    public static void main(String[] args) {
+        identifyOddEven(15);
+
+        System.out.println(identifyOddEven2(10));
+    }
+
 }
 /*
     Write  a method which can identifies given number is even or odd
